cf19555b09566532ac2d2d666d9ea6b528fd24a0
Satish <satish@promobitech.com>
2015-05-06 16:44:37 +0530

    added grunt configuration for running test

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Fork this repo, and when done issue a pull request for review.
 3. The app must process the form POST, validate the parameters
 4. If there are validation errors, the user should be notified about them
 5. If there are no errors, the app should log the results and save the result to a mongo database
+
+
+# Run app
+1. install the dependencies using 'npm install'
+2. install mongodb and run instance of mongodb
+3. run app using 'grunt' command
+4. to run tests use 'grunt test' command
--- tests/contactus.server.model.test.js
+++ tests/contactus.server.model.test.js
@@ -14,25 +14,32 @@ describe('ContactUs Model unit test:', function(){
             subject: 'this is test',
             message: 'save successfully'
         });
+        done();
     });
-});
 
-describe('Save method', function(){
-    it('should be able to save without errors', function(done){
-        return contactUs.save(function(err){
-            should.not.exist(err);
-            done();
+
+    describe('Save method', function(){
+        it('should be able to save without errors', function(done){
+            return contactUs.save(function(err){
+                should.not.exist(err);
+                done();
+            });
+        });
+
+        it('should be able to show error when try to save without valid value', function(done){
+            contactUs.name = '';
+            contactUs.email = '@!!@sd';
+            contactUs.subject = '';
+            contactUs.message = '';
+            return contactUs.save(function(err){
+                should.exist(err);
+                done();
+            });
         });
     });
 
-    it('should be able to show error when try to save without valid value', function(done){
-        contactUs.name = '';
-        contactUs.email = '@!!@sd';
-        contactUs.subject = '';
-        contactUs.message = '';
-        return contactUs.save(function(err){
-            should.exist(err);
-            done();
-        });
+    afterEach(function(done) {
+        ContactUs.remove().exec();
+        done();
     });
 });
